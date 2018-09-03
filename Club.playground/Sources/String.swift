e0aa36408006d85aadc198fb78aec667fe787105
ShengHuaWu <fantasy0404@gmail.com>
2018-09-03 18:00:36 +0200

    Rewrite algorithms.

--- Club.playground/Sources/String.swift
+++ Club.playground/Sources/String.swift
@@ -1,38 +1,6 @@
 import Foundation
 
 extension String {
-    public func countOfPrettyString(with length: Int) -> Int {
-        guard length > 0 else { return 0 }
-        
-        if length == 1 { return characters.count }
-        
-        var result = 0
-        for index in characters.indices {
-            guard let end = self.index(index, offsetBy: length, limitedBy: endIndex) else { break }
-            
-            let bounds = (index, end)
-            let range = Range(uncheckedBounds: bounds)
-            if isPrettyString(in: range) {
-                result += 1
-            }
-        }
-        
-        return result
-    }
-    
-    private func isPrettyString(in range: Range<Index>) -> Bool {
-        var index = self.index(after: range.lowerBound)
-        while index < range.upperBound {
-            if self[index] != self[self.index(before: index)] {
-                return false
-            }
-            
-            index = self.index(after: index)
-        }
-        
-        return true
-    }
-    
     public func isAnagram(with word: String) -> Bool {
         guard characters.count == word.characters.count else { return false }
         
@@ -70,29 +38,6 @@ extension String {
         }
         
         return true
-    }
-}
-
-// Truncate String Without Fragment
-extension String {
-    public func truncate(with length: Int) -> String {
-        guard !isEmpty, length > 0 else { return "" }
-        
-        var end = index(startIndex, offsetBy: length + 1, limitedBy: endIndex) ?? endIndex
-        while !needsTruncating(at: end) {
-            end = index(before: end)
-        }
-        
-        return substring(to: end)
-    }
-    
-    private func needsTruncating(at index: Index) -> Bool {
-        guard index < endIndex else { return false }
-        
-        switch self[index] {
-        case " ", ".", ",", ";", "?", "!": return true
-        default: return false
-        }
     }
 }
 
