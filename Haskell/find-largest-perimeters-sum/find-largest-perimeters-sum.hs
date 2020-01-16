10b2e89393362e764be83424ebd4aeec08da2a76
ShengHua Wu <fantasy0404@gmail.com>
2020-01-16 22:01:46 +0100

    [Haskell] Update find largest perimeters sum.

--- Haskell/find-largest-perimeters-sum/find-largest-perimeters-sum.hs
+++ Haskell/find-largest-perimeters-sum/find-largest-perimeters-sum.hs
@@ -3,17 +3,14 @@ import Assert
 
 -- The array should be sorted descently beforehand
 findLargestPerimetersSum :: (Num a, Ord a) => [a] -> Maybe a
-findLargestPerimetersSum array = findLargestPerimetersSum_ array 0
-
-findLargestPerimetersSum_ :: (Num a, Ord a) => [a] -> Int -> Maybe a
-findLargestPerimetersSum_ array start 
-    | start + 2 > (length array - 1) = Nothing
+findLargestPerimetersSum xs 
+    | length xs < 3 = Nothing
     | p2 + p3 > p1 = Just (p1 + p2 + p3)
-    | otherwise = findLargestPerimetersSum_ array (start + 1)
-    where 
-        p1 = array !! start
-        p2 = array !! (start + 1)
-        p3 = array !! (start + 2)
+    | otherwise = findLargestPerimetersSum (tail xs)
+    where
+        p1 = head xs
+        p2 = (head . tail) xs
+        p3 = (head . tail . tail) xs
 
 main = do
     assertEq (findLargestPerimetersSum [11, 9, 8, 6, 4, 3, 1]) (Just 28)
