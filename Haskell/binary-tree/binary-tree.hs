5edab4db5efa7cafe80b58aa97b17c27dd9d4d03
ShengHua Wu <fantasy0404@gmail.com>
2020-01-29 21:32:11 +0100

    [Haskell] Implement remove for binary search tree.

--- Haskell/binary-tree/binary-tree.hs
+++ Haskell/binary-tree/binary-tree.hs
@@ -34,7 +34,7 @@ height tree = case tree of
     Leaf -> 0
     Node _ left right -> 1 + max (height left) (height right)
 
-minElement :: Tree Int -> Maybe Int
+minElement :: (Ord a) => Tree a -> Maybe a
 minElement tree = case tree of
     Leaf -> Nothing
     -- maybe :: b -> (a -> b) -> Maybe a -> b
@@ -43,7 +43,7 @@ minElement tree = case tree of
     -- Otherwise, it applies the functraverseInOrdern to the value inside the Just and returns the result.
     Node a left _ -> Just (maybe a (min a) (minElement left))
 
-maxElement :: Tree Int -> Maybe Int
+maxElement :: (Ord a) => Tree a -> Maybe a
 maxElement tree = case tree of
     Leaf -> Nothing
     Node a _ right -> Just (maybe a (max a) (maxElement right))
@@ -70,6 +70,18 @@ invert tree = case tree of
     Leaf -> Leaf
     Node x left right -> Node x (invert right) (invert left)
 
+remove :: (Ord a) => Tree a -> a -> Tree a
+remove Leaf _ = Leaf
+remove (Node x left right) a | x > a = Node x (remove left a) right -- Move to left if value is larger than key
+remove (Node x left right) a | x < a = Node x left (remove right a) -- Move to right if value is smaller than key
+remove (Node x Leaf Leaf) a | x == a = Leaf -- Just remove the node itself because it has no child
+remove (Node x left Leaf) a | x == a = left -- Return left if the node only has left child
+remove (Node x Leaf right) a | x == a = right -- Return right if the node only has right child
+remove (Node x left right) a 
+    | x == a = Node min left (remove right min) -- Return one new node with the minimum value from the right, and remove the minimum value as well
+    where
+        min = (fromJust . minElement) right
+
 main = do
     assertEq (height sampleTree) 5
     assertEq (minElement sampleTree) (Just 2)
@@ -80,3 +92,4 @@ main = do
     assertEq (contain sampleTree 99) False
     assertEq (traverseInOrder sampleTree (\x -> x)) [2,3,5,6,7,10,12,13,18,19,21,23,25]
     assertEq (traverseInOrder (invert sampleTree) (\x -> x)) [25,23,21,19,18,13,12,10,7,6,5,3,2]
+    assertEq (traverseInOrder (remove sampleTree 18) (\x -> x)) [2,3,5,6,7,10,12,13,19,21,23,25]
