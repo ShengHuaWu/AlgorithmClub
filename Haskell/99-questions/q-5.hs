6d9f569f1e6f5a2d769d6f5fda0eee8206f820a9
ShengHua Wu <swu1@wayfair.com>
2021-01-12 09:38:31 +0100

    Append question 3 to 6.

---
+++ Haskell/99-questions/q-5.hs
@@ -0,0 +1,9 @@
+-- Reverse a list
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+main = do
+  print $ myReverse "A man, a plan, a canal, panama!"
+  print $ myReverse [1,2,3,4]
