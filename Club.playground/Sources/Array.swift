cf64144b1db00ddeb74323b350ced55b3fa3766d
ShengHua Wu <fantasy0404@gmail.com>
2021-01-31 21:20:31 +0100

    Append new questions.

--- Club.playground/Sources/Array.swift
+++ Club.playground/Sources/Array.swift
@@ -458,3 +458,36 @@ extension Array where Element == Int {
         return sortedArray
     }
 }
+
+// Find subset from an array of integers
+//
+// Split the original array into two subsets A and B:
+// The intersection of A and B is empty.
+// The union of A and B is the original array.
+// The number of element in A should be as less as possible.
+// The sum of A is greater than the sum of B.
+// A should be increasing order.
+// Write a function to find the subset A.
+extension Array where Element == Int {
+    public func findSubset() -> [Int] {
+        /* first thought:
+         Sort the original array and start from the largest element.
+         However, the time complexity is too high.
+        */
+        fatalError()
+    }
+}
+
+// Find largest size after removal
+//
+// There is a n x m mesh storage and each cell is 1 x 1 size.
+// We are going to remove h number of the horizontal lines and v number of the vertical lines.
+// For instance, n = 3, m = 3, h = [1] and v = [2], then the result is 2.
+// Write a function to find the largest size after removal.
+public func findLargestSizeAfterRemoval(n: Int, m: Int, h: [Int], v: [Int]) -> Int {
+    /* first thought:
+     Find the largest lengh of the adjacent elements of h and v,
+     and the multiplication should be the result ???
+    */
+    fatalError()
+}
