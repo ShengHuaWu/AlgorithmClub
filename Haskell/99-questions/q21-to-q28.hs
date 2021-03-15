30a0004d0ca7677976c6908d6958f7991c434643
ShengHua Wu <swu1@wayfair.com>
2021-03-15 11:47:19 +0100

    Append range quiz.

--- Haskell/99-questions/q21-to-q28.hs
+++ Haskell/99-questions/q21-to-q28.hs
@@ -4,9 +4,17 @@ myInsert x [] _ = [x]
 myInsert x xs 1 = x:xs
 myInsert x (y:ys) n = y:(myInsert x ys (n-1))
 
+-- #22: Create a list containing all integers within a given range.
+myRange :: Int -> Int -> [Int]
+myRange s e = [s..e]
+
 main = do
   print "#21"
   print $ myInsert 'x' "" 9
   print $ myInsert 'x' "aaaabccaadeeee" 1
   print $ myInsert 'x' "aaaabccaadeeee" 3
   print $ myInsert 99 [1, 1, 2, 3, 3, 4, 5, 5] 5
+  print "#22"
+  print $ myRange 4 9
+  print $ myRange 10 20
+  print $ myRange 10 0
