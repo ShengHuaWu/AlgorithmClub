c59f853255f09d390706f7dc868c4e5e94ca91c5
ShengHua Wu <swu1@wayfair.com>
2021-12-08 11:55:22 +0100

    Append test cases for multiple customers.

--- Club.playground/Contents.swift
+++ Club.playground/Contents.swift
@@ -820,7 +820,88 @@ final class TDD_RateLimitTests: XCTestCase {
         XCTAssertNotNil(api.invokeEndpoint(customer2))
     }
     
-    // TODO: write tests for multiple customers
+    func testInvokeEndpointFiveTimesForFirstCustomerButSixTimesForSecondCustomer() {
+        let customer1 = "ABC"
+        let customer2 = "XYZ"
+        
+        let api = TDD_API()
+        
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNil(api.invokeEndpoint(customer2))
+    }
+    
+    func testInvokeEndpointSixTimesWithTwoCustomers() {
+        let customer1 = "ABC"
+        let customer2 = "XYZ"
+        
+        let api = TDD_API()
+        
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNil(api.invokeEndpoint(customer1))
+        
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNil(api.invokeEndpoint(customer2))
+    }
+    
+    func testInvokeEndpointSixTimesWithTwoCustomersButExceedingTwoSeconds() {
+        let customer1 = "ABC"
+        let customer2 = "XYZ"
+        
+        let api = TDD_API()
+        
+        let delayTwoSeconds = expectation(description: #function)
+        
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        XCTAssertNotNil(api.invokeEndpoint(customer1))
+        
+        let group = DispatchGroup()
+        let queue = DispatchQueue(label: #function)
+        
+        group.enter()
+        queue.asyncAfter(deadline: .now() + 2) {
+            XCTAssertNotNil(api.invokeEndpoint(customer1))
+            group.leave()
+        }
+        
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        XCTAssertNotNil(api.invokeEndpoint(customer2))
+        
+        group.enter()
+        queue.asyncAfter(deadline: .now() + 2) {
+            XCTAssertNotNil(api.invokeEndpoint(customer2))
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            delayTwoSeconds.fulfill()
+        }
+        
+        wait(for: [delayTwoSeconds], timeout: 5.0)
+    }
 }
 
 //TDD_RateLimitTests.defaultTestSuite.run()
