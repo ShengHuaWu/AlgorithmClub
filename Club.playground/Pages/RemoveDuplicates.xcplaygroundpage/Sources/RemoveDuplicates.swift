20cd4cd380e7f6e6d1d1b26e6d6cc81e756d7c70
ShengHua Wu <swu1@wayfair.com>
2021-12-15 13:06:16 +0100

    Update the logic of removing duplicates for a large file.

--- Club.playground/Pages/RemoveDuplicates.xcplaygroundpage/Sources/RemoveDuplicates.swift
+++ Club.playground/Pages/RemoveDuplicates.xcplaygroundpage/Sources/RemoveDuplicates.swift
@@ -22,9 +22,17 @@ extension Array where Element: Hashable {
     // remove duplications with any new order
     //
     // 1. Divide the list into small pieces which can be loaded into the memory.
-    // 2. Store every pieces
-    // 3. Load the first lines of the two pieces.
-    //    If they are the same, then only store one into the result.
-    //    If thet are NOT the same, then store both into the result.
-    // 4. Repeat above the comparison between the result and the reminding pieces.
+    // 2. Sort and remove duplicates for each small piece
+    // 3. Store each small piece
+    // 4. Load the first element of the two pieces A and B:
+    //    If they are the same,
+    //    then only store one into the result,
+    //    and move to the next elements of both A and B
+    //
+    //    If thet are NOT the same,
+    //    then only store the acending (small) one into the result,
+    //    and move to the next element only of the acending one
+    //
+    //    If there are reminding elements of A or B, store them into the result
+    // 5. Repeat above the comparison between the result and the reminding pieces.
 }
